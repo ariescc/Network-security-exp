ece6e31ab0f9b8ed075304a5e2fe941ec2983e3e
ariescc <jiachang@mail.nwpu.edu.cn>
2018-06-05 18:31:58 +0800

    凯撒加密算法

---
+++ ex1/Caresar.cpp
@@ -0,0 +1,58 @@
+#define LOCAL
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+string lower_letter = "abcdefghijklmnopqrstuvwxyz";
+string upper_letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+int main()
+{
+
+    // read the key from the user
+    int key;
+    bool flag = false; // plaintext
+    cin >> key;
+
+    // read the plaintext from the user
+
+    #ifdef LOCAL
+    freopen("email.txt", "r", stdin);
+    freopen("ciphertext.txt", "w", stdout);
+    #endif // LOCAL
+
+    string text;
+
+    while(getline(cin, text)) {
+        for(int i = 0; i < text.length(); i++) {
+            if(text[i] != '\r' && text[i] != '\n' &&
+               text[i] != ' ') {
+                if(text[i] > 'a' && text[i] < 'z') {
+                    if(key >= 0) {
+                        cout << lower_letter[(text[i]-'a'+key)%26];
+                    }
+                    else {
+                        cout << lower_letter[(text[i]-'a'+26+key)%26];
+                    }
+
+                }
+                else if(text[i] > 'A' && text[i] < 'Z') {
+                    if(key >= 0) {
+                        cout << upper_letter[(text[i]-'A'+key)%26];
+                    }
+                    else {
+                        cout << upper_letter[(text[i]-'A'+26+key)%26];
+                    }
+                }
+               }
+            else {
+                cout << text[i];
+            }
+        }
+    }
+
+    return 0;
+}
